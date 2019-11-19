b6f8e095b51a81174d43c2389c2361d1d257b01c
sherinshibu <56407455+sherinshibu@users.noreply.github.com>
2019-11-19 20:05:47 +0000

    Attempt fizzbuzz 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,18 @@
 
 // More Information about this assisngment is available in the README.md file
 
+for ( var i = 1; i <= 100; i++  ){
+
+    if ( i % 3 == 0 && i % 5 == 0 ){
+        output("FizzBuzz");
+    }
+    else if ( i % 3 == 0){
+        output("Fizz");
+    }
+    else if( i % 5 == 0 ){
+        output("Buzz");
+    }
+    else {
+        output( i );
+    }
+}
